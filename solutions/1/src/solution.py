e19578207aba9519902c4858fcd84abb6def1552
Kevin Sun <kevin.sun@kbc.be>
2022-12-06 18:43:35 +0100

    Sunmit cursed solution

--- solutions/1/src/solution.py
+++ solutions/1/src/solution.py
@@ -1,22 +1,30 @@
-def collect_entries(lines):
-    l = []
-    ll = []
-    for line in lines:
-        if line == "\n":
-            l.append(ll)
-            ll = []
-            continue
-        ll.append(int(line.rstrip()))
-    return l
+from functools import reduce
 
 
-def get_max_calories(file_name, n):
+def sum_entries(lines):
+    l = [
+        list(filter(None, s.split("\n")))
+        for s in reduce(lambda a, b: a + b, lines).split("\n\n")
+    ]
+    return [sum(map(lambda a: int(a), ll)) for ll in l]
+
+
+def get_max_calories(file_name):
     with open(file_name, "r") as f:
         lines = f.readlines()
-        p = collect_entries(lines)
-        summed = [sum(elf) for elf in p]
+        summed = sum_entries(lines)
+        return max(summed)
+
+
+def get_max_calories_for_top_n(file_name, n):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        summed = sum_entries(lines)
         return sum(sorted(summed, reverse=True)[:n])
 
 
-calories = get_max_calories("input.txt", 3)
+calories = get_max_calories("../input.txt")
+print(calories)
+
+calories = get_max_calories_for_top_n("../input.txt", 3)
 print(calories)
