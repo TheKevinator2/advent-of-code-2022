c98adaa9ee17eeee20bb8a321e498fe5d27bbae9
Kevin Sun <kevin.sun@kbc.be>
2022-12-06 18:51:24 +0100

    Submit proper cursed solution

--- solutions/1/src/solution.py
+++ solutions/1/src/solution.py
@@ -1,26 +1,26 @@
 from functools import reduce
 
 
-def sum_entries(lines):
+def collect_entries(lines):
     l = [
         list(filter(None, s.split("\n")))
         for s in reduce(lambda a, b: a + b, lines).split("\n\n")
     ]
-    return [sum(map(lambda a: int(a), ll)) for ll in l]
+    return [list(map(lambda a: int(a), ll)) for ll in l]
 
 
 def get_max_calories(file_name):
     with open(file_name, "r") as f:
         lines = f.readlines()
-        summed = sum_entries(lines)
-        return max(summed)
+        entries = collect_entries(lines)
+        return max([sum(entry) for entry in entries])
 
 
 def get_max_calories_for_top_n(file_name, n):
     with open(file_name, "r") as f:
         lines = f.readlines()
-        summed = sum_entries(lines)
-        return sum(sorted(summed, reverse=True)[:n])
+        entries = collect_entries(lines)
+        return sum(sorted([sum(entry) for entry in entries], reverse=True)[:n])
 
 
 calories = get_max_calories("../input.txt")
